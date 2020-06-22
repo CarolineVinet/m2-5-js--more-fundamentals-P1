a07183eead3071843bcd619ca3cc9fbb90e6ec16
Caroline Vinet <carovinet@gmail.com>
2020-06-22 14:10:25 -0400

    exercise2.4

--- workshop/exercises-P2_tests/exercise-2-4.js
+++ workshop/exercises-P2_tests/exercise-2-4.js
@@ -10,12 +10,25 @@
 // getLetterAtIndex("abc", 0);   // a
 
 function getLetterAtIndex(str, index) {
-  // Your code here
+  let position = str.charAt(index);
+  switch (str) {
+    case "":
+      return undefined;
+  }
+  return position;
 }
 
+console.log(getLetterAtIndex("pitbull", 3));
+
 // Add 6 more test cases
-expect(getLetterAtIndex('hello', 4), 'o');
-expect(getLetterAtIndex('goodbye', 0), 'g');
+expect(getLetterAtIndex("hello", 4), "o");
+expect(getLetterAtIndex("goodbye", 0), "g");
+expect(getLetterAtIndex("Caroline", 6), "n");
+expect(getLetterAtIndex("", 12), undefined);
+expect(getLetterAtIndex("goodbye", 5), "y");
+expect(getLetterAtIndex("water", 0), "w");
+expect(getLetterAtIndex("javascript", 4), "s");
+expect(getLetterAtIndex("lol", 0), "l");
 
 /**
  * -------------------------------------------------------------------
@@ -24,7 +37,7 @@ expect(getLetterAtIndex('goodbye', 0), 'g');
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
